2b81ef4df4cb8308301a0a0f8621b0da3da5ef5b
resilientpathwaysbusiness <129128044+resilientpathwaysbusiness@users.noreply.github.com>
2023-03-27 18:54:11 -0700

    Create conversation.sql

---
+++ conversation.sql
@@ -0,0 +1,25 @@
+              * Positive and welcoming tone
+                |
+                |
+                |
+                |
+                |
+                |
+                |
+* Encourage everyone to participate   * Choose a topic that everyone can engage with
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+* Ask open-ended questions           * Listen actively and show empathy
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+      |                                    |
+* Be respectful and avoid judgement   * End on a positive note
